3c40ab42dc3e46ee2791e34509f3900bf408dcdc
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-05-22 14:22:05 +0000

    Carregamento de dados via API com Fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,50 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  const java = {
-    nome: 'Java',
-    imagem: 'https://www.salvatore.academy/devmon/1_java.png',
-    evoluiPara: 'Kotlin'
+  // const java = {
+  //   nome: 'Java',
+  //   imagem: 'https://www.salvatore.academy/devmon/1_java.png',
+  //   evoluiPara: 'Kotlin'
+  // }
+
+  // const kotlin = {
+  //   nome: 'Kotlin',
+  //   imagem: 'https://salvatore.academy/devmon/2_kotlin.png',
+  //   evoluiPara: 'Android'
+  // }
+
+  // const android = {
+  //   nome: 'Android',
+  //   imagem: 'https://salvatore.academy/devmon/3_android.png'
+  // }
+
+  // const c = {
+  //   nome: 'C',
+  //   imagem: 'https://salvatore.academy/devmon/4_c.png',
+  //   evoluiPara: 'C++'
+  // }
+
+  // const cplusplus = {
+  //   nome: 'C++',
+  //   imagem: 'https://salvatore.academy/devmon/5_cpp.png',
+  //   evoluiPara: 'C#'
+  // }
+
+  // const devmons = [java, kotlin, android, c, cplusplus]
+
+  const devmons = []
+
+  async function fetchData() {
+    const apiUrl = 'https://backend-iniciante-integrar-com-frontend-uyiz.onrender.com/personagem'
+  
+    const response = await fetch(apiUrl)
+  
+    const data = await response.json()
+
+    console.log(45,response)
   }
 
-  const kotlin = {
-    nome: 'Kotlin',
-    imagem: 'https://salvatore.academy/devmon/2_kotlin.png',
-    evoluiPara: 'Android'
-  }
-
-  const android = {
-    nome: 'Android',
-    imagem: 'https://salvatore.academy/devmon/3_android.png'
-  }
-
-  const c = {
-    nome: 'C',
-    imagem: 'https://salvatore.academy/devmon/4_c.png',
-    evoluiPara: 'C++'
-  }
-
-  const cplusplus = {
-    nome: 'C++',
-    imagem: 'https://salvatore.academy/devmon/5_cpp.png',
-    evoluiPara: 'C#'
-  }
-
-  const devmons = [java, kotlin, android, c, cplusplus]
+  fetchData()
 
   return (
     <>
