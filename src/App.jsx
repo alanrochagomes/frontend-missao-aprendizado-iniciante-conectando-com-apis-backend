ab6260dfe4d4df3cc5875d486e13ee4e10c4d828
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-05-22 14:46:20 +0000

    Criação do estado `devmons` recebendo valores da API (`useState` e `useEffect`)

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  // const java = {
-  //   nome: 'Java',
-  //   imagem: 'https://www.salvatore.academy/devmon/1_java.png',
-  //   evoluiPara: 'Kotlin'
-  // }
-
-  // const kotlin = {
-  //   nome: 'Kotlin',
-  //   imagem: 'https://salvatore.academy/devmon/2_kotlin.png',
-  //   evoluiPara: 'Android'
-  // }
-
-  // const android = {
-  //   nome: 'Android',
-  //   imagem: 'https://salvatore.academy/devmon/3_android.png'
-  // }
-
-  // const c = {
-  //   nome: 'C',
-  //   imagem: 'https://salvatore.academy/devmon/4_c.png',
-  //   evoluiPara: 'C++'
-  // }
-
-  // const cplusplus = {
-  //   nome: 'C++',
-  //   imagem: 'https://salvatore.academy/devmon/5_cpp.png',
-  //   evoluiPara: 'C#'
-  // }
-
-  // const devmons = [java, kotlin, android, c, cplusplus]
-
-  const devmons = []
+  const [devmons, setDevmons] = useState([])
 
   async function fetchData() {
     const apiUrl = 'https://backend-iniciante-integrar-com-frontend-uyiz.onrender.com/personagem'
@@ -42,10 +12,12 @@ function App() {
   
     const data = await response.json()
 
-    console.log(45,response)
+    setDevmons(data)
   }
 
-  fetchData()
+  useEffect(function () {
+    fetchData()
+  }, [])
 
   return (
     <>
